5e69a797b9e2faa1c0ad30d0d25abf2dd5cd8271
ilyas <ilyas.laksiri@gmail.com>
2024-06-13 12:08:08 +0200

    Supprimer mon profil pour les users

--- template/header.php
+++ template/header.php
@@ -2,8 +2,10 @@
 <!doctype html>
 <html lang="en">
 
-<?php session_start(); ?>
-
+<?php session_start(); 
+	$access_levels = isset($_SESSION["access_level"]) ? 
+		explode(',', $_SESSION["access_level"]) : array();
+?>
 <head>
 	<link rel="icon" type="image/png" href="images/logo-icon.png" />
 	<meta charset="utf-8"/>
@@ -70,7 +72,20 @@
 	
 	<section id="secondary_bar">
 		<div class="user">
-			<p>Bonjour,  <?php echo $_SESSION["prenom"]; ?> | <a href="ajouter_user.php?id_user=<?php echo $_SESSION["id_user"]; ?>">Mon profil</a></p>
+			
+			<?php
+			// Vérification de l'accès selon le niveau d'utilisateur
+			if (in_array('admin', $access_levels) || in_array('adminRH', $access_levels)) {
+				?>
+				<p>Bonjour, <?php echo $_SESSION["prenom"]; ?> | <a href="ajouter_user.php?id_user=<?php echo $_SESSION["id_user"]; ?>">Mon profil</a></p>
+				<?php
+			} else {
+				?>
+				<p>Bonjour, <?php echo $_SESSION["prenom"]; ?></p>
+				<?php
+			}
+			?>
+			
 			<!-- <a class="logout_user" href="#" title="Logout">Logout</a> -->
 		</div>
 		<div class="breadcrumbs_container">
@@ -143,7 +158,3 @@ fclose($fp);
 return $annee;
 }
 ?> 
-<?php
-	$access_levels = isset($_SESSION["access_level"]) ? 
-		explode(',', $_SESSION["access_level"]) : array();
-?>
